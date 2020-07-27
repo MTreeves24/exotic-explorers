8c2c9f7ec8923cf1059b6c9d37b042b188a0a519
Michael Treeves <mtreeves@pop-os.localdomain>
2020-07-27 14:38:18 +0100

    add new service pages and faq page. Fix font sizing on homepage service panels

--- ee.js
+++ ee.js
@@ -36,7 +36,7 @@ document.querySelectorAll(".read-more-trigger").forEach((item) =>
   })
 );
 
-//////////////////////////////GALLERY///////////////////////////////////
+//////////////////////////////SERVICES PANELS///////////////////////////////////
 
 const panels = document.querySelectorAll(".panel");
 function toggleOpen() {
@@ -51,6 +51,8 @@ panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 panels.forEach((panel) =>
   panel.addEventListener("transitionend", toggleActive)
 );
+
+//////////////////////////////GALLERY///////////////////////////////////
 
 var pswpElement = document.querySelectorAll(".pswp")[0];
 
@@ -258,47 +260,49 @@ var initPhotoSwipeFromDOM = function (gallerySelector) {
 // execute above function
 initPhotoSwipeFromDOM(".my-gallery");
 
-
 //FITTEXT within container
-(function(){
+(function () {
   var addEvent = function (el, type, fn) {
-    if (el.addEventListener)
-      el.addEventListener(type, fn, false);
-		else
-			el.attachEvent('on'+type, fn);
+    if (el.addEventListener) el.addEventListener(type, fn, false);
+    else el.attachEvent("on" + type, fn);
   };
-  var extend = function(obj,ext){
-    for(var key in ext)
-      if(ext.hasOwnProperty(key))
-        obj[key] = ext[key];
+  var extend = function (obj, ext) {
+    for (var key in ext) if (ext.hasOwnProperty(key)) obj[key] = ext[key];
     return obj;
   };
   window.fitText = function (el, kompressor, options) {
-    var settings = extend({
-      'minFontSize' : -1/0,
-      'maxFontSize' : 1/0
-    },options);
+    var settings = extend(
+      {
+        minFontSize: -1 / 0,
+        maxFontSize: 1 / 0,
+      },
+      options
+    );
     var fit = function (el) {
       var compressor = kompressor || 1;
       var resizer = function () {
-        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+        el.style.fontSize =
+          Math.max(
+            Math.min(
+              el.clientWidth / (compressor * 10),
+              parseFloat(settings.maxFontSize)
+            ),
+            parseFloat(settings.minFontSize)
+          ) + "px";
       };
       // Call once to set.
       resizer();
       // Bind events
       // If you have any js library which support Events, replace this part
       // and remove addEvent function (or use original jQuery version)
-      addEvent(window, 'resize', resizer);
-      addEvent(window, 'orientationchange', resizer);
+      addEvent(window, "resize", resizer);
+      addEvent(window, "orientationchange", resizer);
     };
-    if (el.length)
-      for(var i=0; i<el.length; i++)
-        fit(el[i]);
-    else
-      fit(el);
+    if (el.length) for (var i = 0; i < el.length; i++) fit(el[i]);
+    else fit(el);
     // return set of elements
     return el;
   };
 })();
 
-window.fitText( document.querySelectorAll(".fittext"), 0.9);
+window.fitText(document.querySelectorAll(".fittext"), 0.9);
